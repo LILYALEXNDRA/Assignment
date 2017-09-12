6a68b30999457baa87f89c5477694ecc8e60ae72
LILYALEXNDRA <31762079+LILYALEXNDRA@users.noreply.github.com>
2017-09-12 12:10:56 +0800

    Update Chap01Dubos.java

--- Chap01Dubos.java
+++ Chap01Dubos.java
@@ -7,7 +7,7 @@ public class Chap01Dubos {
 		System.out.println("\"Put down your pencil, Candidate Alexandra.\"\n\tScared Face");
 		System.out.print("\n1.4 Class, method, statement and comment\nHello,World!\n\n1.5 \"print\" versus \"println\"\nHello World!\nThis example also shows the escape sequence \"\\\"\n\n1.6.1 The escape sequence \"\\n\"\n(1) Hello,\nWorld!\n(2) Hello,/");
 		System.out.print("nWorld!\n\n1.6.2 The escape sequence: \"\\");
-		System.out.print("t\nHello,   World!\nHello,\n\tWorld!\n(1)Hello,\tWorld!\n(2)Hello,\n\tWorld!\n\n(5) He said:\"Hello, World!\"\n Hello, World!\\\\");
+		System.out.print("t\nHello,   World!\nHello,\n\tWorld!\n(1)Hello,\tWorld!\n(2)Hello,\n\tWorld!\n\n(5) He said:\"Hello, World!\"\n(6) Hello, World!\\\\");
 	}
 }
 
